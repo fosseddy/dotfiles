58b8b52266718719818a95532e68e5fc00ffd6a2
fosseddy <markfosseddy@gmail.com>
2024-06-03 15:27:32 +0500

    update xorg

--- .config/xorg/setup.sh
+++ .config/xorg/setup.sh
@@ -5,7 +5,7 @@ xinput set-prop "ELAN2602:00 04F3:3109 Touchpad" "libinput Tapping Enabled" 1
 xinput set-prop "ELAN2602:00 04F3:3109 Touchpad" "libinput Natural Scrolling Enabled" 1
 
 xinput set-prop "Logitech USB Optical Mouse" "libinput Natural Scrolling Enabled" 1
-xinput set-prop "Logitech USB Optical Mouse" "libinput Accel Speed" -0.4
+xinput set-prop "Logitech USB Optical Mouse" "libinput Accel Speed" -0.6
 
 setxkbmap -option caps:escape
 setxkbmap -model pc104 -layout us,ru -option grp:rctrl_rshift_toggle
